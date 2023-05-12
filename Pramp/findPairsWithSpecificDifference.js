08d86b3c095fc25fd655670a247dbfba8bfa6274
rtungol <rarrtungol@gmail.com>
2023-05-11 23:08:28 -0400

    fix solution to maintain y value placements

--- Pramp/findPairsWithSpecificDifference.js
+++ Pramp/findPairsWithSpecificDifference.js
@@ -52,36 +52,76 @@ left -1 | right = 2
 Time Complexity -> O(n)
 */
 
+// function findPairsWithGivenDifference(arr, k) {
+//   // your code goes here
+//   let res = [];
+//   let len = arr.length;
+
+//   for (let i = 0; i < len; i++) {
+//     for (let j = len - 1; j >= 0; j--) {
+//       if (arr[i] - arr[j] === k) {
+//         res.push([arr[i], arr[j]]);
+//       }
+//     }
+//   }
+//   return res;
+// }
+
 function findPairsWithGivenDifference(arr, k) {
   // your code goes here
-  let res = [];
-  let len = arr.length;
 
-  for (let i = 0; i < len; i++) {
-    for (let j = len - 1; j >= 0; j--) {
-      if (arr[i] - arr[j] === k) {
-        res.push([arr[i], arr[j]]);
-      }
+  // trying to get k from going through each element in the list
+
+  // making sure that we maintain y element in the original array (making sure that they are in order of when we visit them)
+
+  const hashMap = {};
+
+  const results = [];
+
+  // this populates the hashMap
+  for (let i = 0; i < arr.length; i++) {
+    // x
+    const currentNumber = arr[i];
+
+    // x -y = k
+    // x = k + y
+
+    // -y = k - x
+    // y = x - k
+    const difference = currentNumber - k;
+    hashMap[difference] = currentNumber;
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    // this is y
+    const currentNumber = arr[i];
+
+    // find the difference in the hashMap
+    if (hashMap[currentNumber] !== undefined) {
+      results.push([hashMap[currentNumber], currentNumber]);
     }
   }
-  return res;
+
+  return results;
 }
 
-let test1 = ([0, -1, -2, 2, 1], 1);
-let test2 = ([1, 5, 11, 7], 4);
-let test3 = ([4, 1], 3);
 console.log(
-  `Test1: Input: ${test1} \n Expected: [[1,0],[0,-1],[-1,-2],[2,1]] \n Output: ${util.inspect(
+  `Test1: \n Expected: [[1,0],[0,-1],[-1,-2],[2,1]] \n Output: ${util.inspect(
     findPairsWithGivenDifference([0, -1, -2, 2, 1], 1)
   )}`
 );
 console.log(
-  `Test2: Input: ${test2} \n Expected: [[7,1],[11,5]] \n Output: ${util.inspect(
+  `Test2: \n Expected: [[5,1],[11,7]] \n Output: ${util.inspect(
     findPairsWithGivenDifference([1, 5, 11, 7], 4)
   )}`
 );
 console.log(
-  `Test3: Input: ${test3} \n Expected: [[4,1]] \n Output: ${util.inspect(
+  `Test3: \n Expected: [[7,1],[11,5]] \n Output: ${util.inspect(
+    findPairsWithGivenDifference([1, 5, 11, 7], 6)
+  )}`
+);
+console.log(
+  `Test4: \n Expected: [[4,1]] \n Output: ${util.inspect(
     findPairsWithGivenDifference([4, 1], 3)
   )}`
 );
